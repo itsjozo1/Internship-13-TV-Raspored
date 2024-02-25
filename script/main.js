630038e430eff6b27c30d1dbfc7202cda3831e20
rokoperisic <roko.perisic@gmail.com>
2024-02-25 22:57:48 +0100

    Parential password feature

--- script/main.js
+++ script/main.js
@@ -1,6 +1,8 @@
 import { tvChannels } from "./channels.js";
 import { tvPrograms } from "./programs.js";
 
+const PARENT_PASSWORD = 123456;
+
 const getProgramsByChannel = (channelId) => {
     return tvPrograms
         .filter(item => item.channelId === channelId)
@@ -34,7 +36,7 @@ const createProgramCard = (tvProgram, container, currentTimeInMinutes) => {
         <p class="program-headline"><b>${tvProgram.title}</b></p>
     `;
 
-    if (currentTimeInMinutes >= startTime && currentTimeInMinutes <= endTime) {
+    if (currentTimeInMinutes >= startTime && currentTimeInMinutes < endTime) {
         programCard.style.backgroundColor = "rgb(243 243 243)"; 
     }
 
@@ -46,29 +48,71 @@ const createProgramCard = (tvProgram, container, currentTimeInMinutes) => {
     };
 };
 
+const checkIsAdultProgram = (tvProgram) => tvProgram.genre.includes("Program za odrasle") ? true : false;
+
+
 const removeExistingProgramDetails = () => {
     const existingDetailsCards = document.querySelectorAll(".program-details-container");
     existingDetailsCards.forEach(card => card.parentNode.removeChild(card));
 };
 
+const createAdultProgramDetails = (programContainer) => {
+    const htmlAdultProgram = `
+    <span class="program-details-close-button">&#10005;</span>
+    <h2 class="adult-program-headline">Roditeljska zaštita</h2>
+    <p>Za informacije o programu unesite pin</p>
+    <div class="adult-program-input-holder">
+        <input type="password" class="parent-password-input"></input>
+        <button class="parent-password-button">Potvrda</button>
+    </div>
+    `;
+    programContainer.classList.add("adult-program-container");
+    programContainer.innerHTML = htmlAdultProgram;
+
+}
+
 const createProgramDetails = (tvProgram) => {
     const programDetails = document.createElement("div");
     programDetails.classList.add("program-details-container");
-    programDetails.innerHTML = `
-        <span class="program-details-close-button">&#10005;</span>
-        <img src="${tvProgram.image}" alt="" class="program-details-image">
-        <div class="program-details-text-container">
-            <p>${tvProgram.genre}</p>
-            <h3 class="program-details-headline">${tvProgram.title}</h3>
-            <p>${tvProgram.startTime} - ${tvProgram.endTime}</p>
-            <p>${tvProgram.desc}</p>
-        </div>
-    `;
+
+    const htmlNormalProgram = `
+    <span class="program-details-close-button">&#10005;</span>
+    <img src="${tvProgram.image}" alt="" class="program-details-image">
+    <div class="program-details-text-container">
+        <p>${tvProgram.genre}</p>
+        <h3 class="program-details-headline">${tvProgram.title}</h3>
+        <p>${tvProgram.startTime} - ${tvProgram.endTime}</p>
+        <p class="program-details-desc">${tvProgram.desc}</p>
+        <button class="more-details-button">Details</body>
+    </div>
+`;
+
+    checkIsAdultProgram(tvProgram) == true ? createAdultProgramDetails(programDetails) : programDetails.innerHTML = htmlNormalProgram;
     document.querySelector("body").appendChild(programDetails);
 
     const closeButton = programDetails.querySelector(".program-details-close-button"); 
     closeButton.onclick = () => {
         programDetails.parentNode.removeChild(programDetails); 
+    };
+
+    const parentPasswordButton = document.querySelector(".parent-password-button");
+    parentPasswordButton.onclick = () => {
+        let inputParentPassword = document.querySelector(".parent-password-input");
+
+        if (inputParentPassword.value == PARENT_PASSWORD) {
+
+            programDetails.innerHTML = htmlNormalProgram;
+            programDetails.classList.remove("adult-program-container")
+
+            const closeButton = programDetails.querySelector(".program-details-close-button"); 
+            closeButton.onclick = () => {
+                programDetails.parentNode.removeChild(programDetails); 
+            };
+        } else {
+            inputParentPassword.style = "border: 1px solid red; border-radius: 5px";
+            inputParentPassword.value = "";
+            inputParentPassword.placeholder = "Pogrešan pin";
+        }
     };
 };
 
@@ -100,4 +144,4 @@ const scrollOffset = currentHourPosition / 1440 * channelsContainer.scrollWidth;
 console.log(currentPosition);
 
 channelsContainer.scrollLeft = scrollOffset;
-realTimeLine.style.left = `calc(${currentPosition*2}% + 1.5rem)`;
+realTimeLine.style.left = `calc(${currentPosition*2}% + 1.25rem)`;
